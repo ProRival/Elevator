2b398d39f4dfdb831524bb7037380c7f4b75555a
Kyle Posluns <ProRival@users.noreply.github.com>
2017-12-04 14:14:18 -0600

    Implemented RobotC motor functions.

--- Elevator.c
+++ Elevator.c
@@ -1,24 +1,27 @@
-#pragma config(Sensor, dgtl1,  button1, sensorTouch)
-#pragma config(Sensor, dgtl2,  button2, sensorTouch)
-#pragma config(Sensor, dgtl3,  button3, sensorTouch)
-#pragma config(Sensor, dgtl4,  killSwitch, sensorTouch)
-#pragma config(Sensor, dgtl4,  sonar, sensorSONAR_cm)
-#pragma config(Motor,  port2,  elevatorMotorLeft, tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port3,  elevatorMotorRight, tmotorVex393_MC29, openLoop)
+#pragma config(Sensor, dgtl1,  button1,        sensorTouch)
+#pragma config(Sensor, dgtl2,  button2,        sensorTouch)
+#pragma config(Sensor, dgtl3,  button3,        sensorTouch)
+#pragma config(Sensor, dgtl4,  sonar,          sensorSONAR_cm)
+#pragma config(Sensor, dgtl9,  killSwitch,     sensorTouch)
+#pragma config(Motor,  port3,  elevatorMotor, tmotorVex393_MC29, openLoop)
 
-//Headers for the main program.
-void moveElevator(int floor, int speed);
-void run();
+/*
+ * Because RobotC does not contain malloc functions, queues cannot have a dynamic size.
+ */
+#define MAX 6
 
 /**
  * Creates a Queue struct.
  */
-typedef struct {
+typedef struct Queue {
 	int intArray[MAX];
 	int front;
 	int rear;
 	int itemCount;
 }Queue;
+
+Queue queue;
+int currentFloor;
 
 //Headers for Queue.
 int peek(Queue queue);
@@ -28,12 +31,14 @@ int size(Queue queue);
 void insert(Queue queue, int data);
 int poll(Queue queue);
 
-Queue queue;
+//Headers for the main program.
+void moveElevator(int floor, int speed);
+void run();
 
 task buttonTask() {
 	while (true) {
 		if (SensorValue(killSwitch) == 1) {
-			//TODO: End program.
+			stopAllTasks();
 			break;
 		}
 
@@ -49,26 +54,34 @@ task buttonTask() {
 
 }
 
+
+
 task main() {
 	//initialize queue data.
 	queue.front = 0;
 	queue.rear = -1;
 	queue.itemCount = 0;
 
+	currentFloor = 0;
+
 	//start listening for button pushes asynchronously.
 	startTask(buttonTask);
 
 	while (true) {
 		if (isEmpty(queue)) continue;
-		//TODO: Start elevator.
+		moveElevator(poll(queue), 20);
 	}
 
 }
 
-/*
- * Because RobotC does not contain malloc functions, queues cannot have a dynamic size.
- */
-#define MAX 6
+void moveElevator(int floor, int speed) {
+	if (currentFloor == floor || floor < 0 || floor > 2) return;							//checks to see if the elevator is already on the current floor and to make sure the floor is valid.
+	while (SensorValue(sonar) != (40 - (15 * floor))) {													//move elevator while the elevator is not on the correct floor.
+ 		startMotor(elevatorMotor, (floor < currentFloor ? speed : -1 * speed)); //make sure the motor turns the correct direction.
+ 	}
+ 	currentFloor = floor;																											//when finished, set the current floor.
+ 	stopMotor(elevatorMotor);
+ }
 
 /**
  * Returns the front value of the queue without removal.
@@ -109,7 +122,7 @@ void insert(Queue queue, int data) {
          queue.rear = -1;
       }
 
-      queue.intArray[++rear] = data;
+      queue.intArray[++queue.rear] = data;
       queue.itemCount++;
    }
 }
